12c1623d7caf116a0ea7b301a7ac81ea50249504
rafaelorland <rafael.orlando2023@gmail.com>
2025-05-31 20:29:42 -0300

    feat: create model product

---
+++ model/produto.py
@@ -0,0 +1,9 @@
+
+class Product:
+    sku: int
+    title: str
+    price: float
+    price_in_pix: float
+    installment_value: float
+    quantity_installment: int
+    description_technique: str
