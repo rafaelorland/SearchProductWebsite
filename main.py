598d4f99b35a6be17e80d42b2c1266841d538339
rafaelorland <rafael.orlando2023@gmail.com>
2025-06-03 02:45:26 -0300

    feat: interface clean

--- main.py
+++ main.py
@@ -2,24 +2,28 @@ from scraper.scraper import MaetoScraper
 from database.db import Database
 
 def main():
-    print("Buscador de Produtos Loja Maeto")
-    while True:
-        search_term = input("Digite o produto de busca: ").strip()
+    print(("="*6)+" Buscador de Produtos - Loja Maeto "+("="*6))
     
+    while True:
+        search_term = input("Digite o nome do produto para buscar ou digite (sair) para encerrar: ").strip()
+
+        if search_term.lower() == 'sair':
+            print(("="*6)+"Encerrando o buscador."+("="*6))
+            break
+
         if not search_term:
-            print("A busca não pode ser vazio.")
-            return
+            print("O termo de busca não pode estar vazio.")
+            continue
         
         db = Database()
         scraper = MaetoScraper(db)
         
         try:
-            
             products = scraper.search_products(search_term)
             if products:
                 db.save_products(products)
             else:
-                print("Nenhum produto encontrado.")
+                print("Nenhum produto encontrado para essa busca.")
         finally:
             db.close()
 
